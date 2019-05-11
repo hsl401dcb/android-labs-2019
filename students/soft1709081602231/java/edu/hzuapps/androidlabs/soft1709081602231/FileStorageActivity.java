be3024c18ccd752c78579e038c62ec913ecedd3f
hsl401dcb <594391417@qq.com>
2019-05-11 22:04:58 +0800

    #8 #578 整合所有实验

--- students/soft1709081602231/java/edu/hzuapps/androidlabs/soft1709081602231/FileStorageActivity.java
+++ students/soft1709081602231/java/edu/hzuapps/androidlabs/soft1709081602231/FileStorageActivity.java
@@ -1,11 +1,14 @@
 package edu.hzuapps.androidlabs.soft1709081602231;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Scroller;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
@@ -13,15 +16,19 @@ import java.io.FileOutputStream;
 
 public class FileStorageActivity extends AppCompatActivity {
     private EditText editText;
-    private Button save,read;
+    private Button save,read,home;
+    private TextView textView2;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.file_storage_activity);
         editText=findViewById(R.id.editText);
         save=findViewById(R.id.save);
         read=findViewById(R.id.read);
+        home=findViewById(R.id.home);
+        textView2=findViewById(R.id.textView2);
         save.setOnClickListener(new ButtonListener());
         read.setOnClickListener(new ButtonListener());
+        home.setOnClickListener(new ButtonListener());
     }
     private class ButtonListener implements View.OnClickListener {
         public void onClick(View v){
@@ -30,13 +37,13 @@ public class FileStorageActivity extends AppCompatActivity {
                     String saveinfo=editText.getText().toString().trim();
                     FileOutputStream fos;
                     try{
-                        fos=openFileOutput("data.txt", Context.MODE_APPEND);
+                        fos=openFileOutput("data.txt", Context.MODE_PRIVATE);
                         fos.write(saveinfo.getBytes());
                         fos.close();
                     }catch (Exception e){
                         e.printStackTrace();
                     }
-                    Toast.makeText(FileStorageActivity.this,"数据保存成功",0).show();
+                    Toast.makeText(FileStorageActivity.this,"修改成功",0).show();
                     break;
                 case R.id.read:
                     String content="";
@@ -45,11 +52,17 @@ public class FileStorageActivity extends AppCompatActivity {
                         byte[] buffer=new byte[fis.available()];
                         fis.read(buffer);
                         content=new String(buffer);
+                        String content1 = content; //从edit text中获取内容
+                        textView2.setText(content1); // 在textview中显示.
                         fis.close();
                     }catch (Exception e){
                         e.printStackTrace();
                     }
-                    Toast.makeText(FileStorageActivity.this,"保存的数据是："+content,0).show();
+                    break;
+                case R.id.home:
+                    Intent a = new Intent(FileStorageActivity.this,Soft1709081602231Activity.class);
+
+                    startActivity(a);
                     break;
                     default:
                     break;
